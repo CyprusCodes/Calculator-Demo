f8ffe5f15892f28405d1d9c13ea5f112bed4dfc0
ahmet92akinsel <ahmet.akinsel@gmail.com>
2021-09-14 19:44:03 +0300

    Calculator Demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useState } from "react";
+import MyButton from "./MyButton";
 
 function App() {
+  const [total, setTotal] = useState("0");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="calculator-container">
+        Total:{total}
+        <div>
+          <MyButton setTotal={setTotal} total={total} digit={1} />
+          <MyButton setTotal={setTotal} total={total} digit={2} />
+          <MyButton setTotal={setTotal} total={total} digit={3} />
+        </div>
+        <div>
+          <MyButton setTotal={setTotal} total={total} digit={4} />
+          <MyButton setTotal={setTotal} total={total} digit={5} />
+          <MyButton setTotal={setTotal} total={total} digit={6} />
+        </div>
+        <div>
+          <MyButton setTotal={setTotal} total={total} digit={7} />
+          <MyButton setTotal={setTotal} total={total} digit={8} />
+          <MyButton setTotal={setTotal} total={total} digit={9} />
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              setTotal("0");
+            }}
+            value={total}
+          >
+            Clear
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
